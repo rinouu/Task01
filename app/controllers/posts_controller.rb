fee0405e288908e452d78a9363d2066ebbbbca10
Corine <corine@localhost.localdomain>
2021-03-26 00:19:11 +0100

    Suppression de post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-	before_action :set_post, only: [:edit, :update, :show]
+	before_action :set_post, only: [:edit, :update, :show, :destroy]
 	def index
   		@posts = Post.all
   	end
@@ -38,6 +38,11 @@ class PostsController < ApplicationController
   	end
 
   	def show
+  	end
+
+  	def destroy
+	    @post.destroy
+	    redirect_to posts_path, notice:"Post deleted!"
   	end
 
   	private

5eb8a08785db545f005148b1350ef8810b9fc264
Corine <corine@localhost.localdomain>
2021-03-25 23:15:06 +0100

    Montrer beaucoup de détails sur un post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-	before_action :set_post, only: [:edit, :update]
+	before_action :set_post, only: [:edit, :update, :show]
 	def index
   		@posts = Post.all
   	end
@@ -35,6 +35,9 @@ class PostsController < ApplicationController
 	    else
 	      	render :edit
 	    end
+  	end
+
+  	def show
   	end
 
   	private
